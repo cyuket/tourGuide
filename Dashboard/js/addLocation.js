bab009bd5f7d26735dec5487e1fafb3248f2f386
CY UKET <34509484+cyuket@users.noreply.github.com>
2019-03-01 23:51:33 +0100

    Update addLocation.js

--- Dashboard/js/addLocation.js
+++ Dashboard/js/addLocation.js
@@ -1,3 +1,17 @@
+var config = {
+    apiKey: "AIzaSyCypRAWS-lk8taWYUUgvNDWCdxabyRclIc",
+    authDomain: "travel-28.firebaseapp.com",
+    databaseURL: "https://travel-28.firebaseio.com",
+    projectId: "travel-28",
+    storageBucket: "travel-28.appspot.com",
+    messagingSenderId: "304303818510"
+  };
+  firebase.initializeApp(config);
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+  const functions = firebase.functions();
+  const storage = firebase.storage();
+
 let featuredImageUrl;
 const upload_widget = document.querySelector("#featuredImage");
 
